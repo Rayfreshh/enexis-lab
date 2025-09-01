af9d1895bd8b94025fbfaa201678eb05451c0633
[Etiosa] <[samsonraymond63@yahoo.com]>
2025-09-01 22:09:36 +0200

    Add health probe annotation and ingress config

--- k8s.tf
+++ k8s.tf
@@ -1,19 +1,51 @@
+resource "kubernetes_namespace" "ingress_nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
 resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
+  namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  namespace  = "ingress-nginx"
+  version    = "4.13.2"
 
-  values = [
-    <<EOF
-controller:
-  service:
-    loadBalancerIP: 50.85.20.181
-    annotations:
-      service.beta.kubernetes.io/azure-load-balancer-resource-group: MC_enexis-lab-rg_enexis-lab-aks_westeurope
-      service.beta.kubernetes.io/azure-load-balancer-health-probe-request-path: /healthz
-EOF
-  ]
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-resource-group"
+    value = "MC_enexis-lab-rg_enexis-lab-aks_westeurope"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+    value = "/healthz"
+  }
+
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = "50.85.20.181"
+  }
+}
+
+resource "kubernetes_service" "enexis_microservice" {
+  metadata {
+    name      = "enexis-microservice-service"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "enexis-microservice"
+    }
+
+    port {
+      port        = 80
+      target_port = 8000
+      protocol    = "TCP"
+    }
+
+    type = "ClusterIP"
+  }
 }
 
 resource "kubernetes_ingress_v1" "enexis_ingress" {
@@ -35,7 +67,7 @@ resource "kubernetes_ingress_v1" "enexis_ingress" {
 
           backend {
             service {
-              name = "enexis-microservice-service"
+              name = kubernetes_service.enexis_microservice.metadata[0].name
               port {
                 number = 80
               }
@@ -44,25 +76,5 @@ resource "kubernetes_ingress_v1" "enexis_ingress" {
         }
       }
     }
-  }
-}
-
-resource "kubernetes_service_v1" "microservice" {
-  metadata {
-    name      = "enexis-microservice-service"
-    namespace = "default"
-  }
-
-  spec {
-    selector = {
-      app = "enexis-microservice"
-    }
-
-    port {
-      port        = 80
-      target_port = 8000
-    }
-
-    type = "ClusterIP"
   }
 }
