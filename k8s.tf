f999a43d446d50510b06a7306448f2a43da7a2db
[Etiosa] <[samsonraymond63@yahoo.com]>
2025-09-01 17:14:15 +0200

    Deploy ingress-nginx in namespace with static IP 50.85.20.181 and ingress rule

--- k8s.tf
+++ k8s.tf
@@ -1,6 +1,12 @@
+resource "kubernetes_namespace" "ingress_nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
 resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
-  namespace  = "ingress-nginx"
+  namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
 
@@ -11,7 +17,7 @@ resource "helm_release" "ingress_nginx" {
 
   set {
     name  = "controller.service.loadBalancerIP"
-    value = "50.85.20.181"   # existing static IP in your node RG
+    value = "50.85.20.181"
   }
 }
 
@@ -20,7 +26,7 @@ resource "kubernetes_ingress_v1" "enexis_ingress" {
     name      = "enexis-ingress"
     namespace = "default"
     annotations = {
-      "kubernetes.io/ingress.class" : "nginx"
+      "kubernetes.io/ingress.class" = "nginx"
     }
   }
 
@@ -28,11 +34,11 @@ resource "kubernetes_ingress_v1" "enexis_ingress" {
     rule {
       http {
         path {
-          path     = "/"
+          path      = "/"
           path_type = "Prefix"
           backend {
             service {
-              name = "example-service"   # replace with your service name
+              name = "example-service"   # replace with your actual service name
               port {
                 number = 80
               }
