8ad2c3ccd9b8462f434f1fee9212fdba0e91f5ae
[Etiosa] <[samsonraymond63@yahoo.com]>
2025-09-01 16:30:59 +0200

    Update providers.tf with latest changes

--- providers.tf
+++ providers.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.3"
+
+  backend "remote" {
+    organization = "RAG"   # your Terraform Cloud org
+    workspaces {
+      name = "enexis-lab"  # your workspace
+    }
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -23,20 +31,4 @@ provider "azurerm" {
   client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
   tenant_id       = var.ARM_TENANT_ID
-}
-
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.enexis_aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.enexis_aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.enexis_aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.enexis_aks.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.enexis_aks.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.enexis_aks.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.enexis_aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.enexis_aks.kube_config[0].cluster_ca_certificate)
-  }
 }
